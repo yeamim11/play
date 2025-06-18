bacb9d349498584bba156d1199940827ab5c6576
yeamim11 <abubokorsiddikyeamim7809@gmail.com>
2025-06-19 01:26:32 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,15 @@ import Navber from './Component/Navbar/Navber'
 import Inicio from './Component/Inicio'
 import Tienda from './Component/Tienda'
 import Contacto from './Component/Contacto'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Component/Home'
+
 
 const App = () => {
   return (
     <div>
-     <Navber/>
-      <Routes>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/inicio' element={ <Inicio/>}/>
-        <Route path='/tienda' element={ <Tienda/>}/>
-        <Route path='/contacto' element={ <Contacto/>}/>
-      </Routes>
+      <Navber />
+      <Inicio />
+      <Tienda />
+      <Contacto />
     </div>
   )
 }
